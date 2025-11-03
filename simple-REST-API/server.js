dc43120bfdbf4bf2e0ef9f423f6e776ddc93cdda
Okoeguale Godwin <okoegualegodwin@gmail.com>
2025-11-03 22:53:04 +0100

    implementing a simple rest

---
+++ simple-REST-API/server.js
@@ -0,0 +1,17 @@
+const express = request("express");
+const app = express();
+
+app.use(express.json());
+
+let movies = [{ id: 1, title: "Inception", year: 2010 }];
+
+app.get("/movies", (req, res) => res.json(movies));
+
+app.post("/movies", (req, res) => {
+  const movie = { id: movies.length + 1, ...req.body };
+  movies.push(movie);
+  res.status(201).json(movie);
+});
+
+app.listen(3000, ()=> console.log('Server running on port 3000');
+)
